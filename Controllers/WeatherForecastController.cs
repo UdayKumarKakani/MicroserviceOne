6a3e5abb5437a638bf26d430b2677cef13226618
Uday Kumar <38879020+UdayKumarKakani@users.noreply.github.com>
2024-07-01 20:58:24 +0530

    Updated forecast controller

--- Controllers/WeatherForecastController.cs
+++ Controllers/WeatherForecastController.cs
@@ -25,7 +25,7 @@ public class WeatherForecastController : ControllerBase
         {
             Date = DateTime.Now.AddDays(index),
             TemperatureC = Random.Shared.Next(-20, 55),
-            Summary = "Data From Microservice One - user Microservice by uday"
+            Summary = "Data From Microservice One - user Microservice by uday Showcase CI capabilities "
         })
         .ToArray();
     }
